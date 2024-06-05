644871ece11e99f8c3268d3ad7bffae891e0cc64
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-05 21:52:28 +0530

    card completed

--- shop/script-shop.js
+++ shop/script-shop.js
@@ -15,10 +15,12 @@ async function getData(url, cat, divDtls) {
   if (cat === "men's clothing" || cat === "women's clothing") {
     let restructuredData = restructureData(data);
     console.log("Restruce", restructuredData);
-    updateUi(restructuredData, divDtls, cat);
+    let shuffledData = shuffleArray(restructuredData);
+    updateUi(shuffledData, divDtls, cat);
   } else {
     console.log("Nm", data);
-    updateUi(data, divDtls, cat);
+    let shuffledData = shuffleArray(data);
+    updateUi(shuffledData, divDtls, cat);
   }
 }
 
@@ -44,6 +46,17 @@ function resetCategoryBg(cat) {
   cat.classList.add("bg-white");
   cat.classList.add("text-black");
 }
+function shuffleArray(arr) {
+  let currIndex = arr.length;
+  while (currIndex !== 0) {
+    let randomIndex = Math.floor(Math.random() * currIndex);
+    currIndex--;
+    let temp = arr[currIndex];
+    arr[currIndex] = arr[randomIndex];
+    arr[randomIndex] = temp;
+  }
+  return arr;
+}
 
 function updateUi(data, divDtls, cat) {
   let title = cat
@@ -57,29 +70,55 @@ function updateUi(data, divDtls, cat) {
   let productDiv = document.getElementById(title);
   data.map((product) => {
     productDiv.innerHTML += `
-    <div class="bg-gray-400 shadow-md hover:scale-105 hover:shadow-xl duration-300 product product cursor-pointer">
-    <div class="prod-img">
-      <img src="${product.image}" class="" />
-    </div>
-    <div>
-      <p class="text-base font-medium text-black capitalize">${
-        product?.title
-      }</p>
-      <div class="flex items-center">
-        <p class="text-base font-semibold my-3 product-price">$${
-          product?.price
-        }</p>
-        <del class="flex items-center">
-          <p class="text-sm pl-3 prev-price">$${Math.floor(
-            product?.price + product?.price * 0.01
-          )}</p>
-        </del>
-      </div>
-      <div>
-          <p>${product?.rating?.rate}</p>
-      </div>
-    </div>
+    <div class="bg-gray-300 py-2 shadow-md hover:shadow-lg hover:shadow-black duration-300 product cursor-pointer relative flex flex-col">
+  <div class="prod-img">
+    <img src="${product.image}" class="" />
   </div>
+  <div class="px-2 flex-1">
+    <p class="text-base font-medium text-black text-justify tracking-tighter capitalize my-1 leading-none">
+      ${product?.title}
+    </p>
+    <div class="w-full flex items-center justify-between">
+      <div>
+        <p class="text-base font-semibold my-1">
+          <span class="prod-curr-price"> $${product?.price} </span>
+          <del class="text-sm text-gray-600 ">
+            <span class="prev-price"> $${Math.floor(product?.price + product?.price * 0.01)} </span>
+          </del>
+        </p>
+       
+      </div>
+      <div class="flex items-center">
+        <p class="mr-1 text-sm text-gray-700">${product?.rating?.rate}</p>
+        <img src="https://cdn-icons-png.flaticon.com/128/477/477406.png" alt="stars" class="h-4 w-4" />
+      </div>
+    </div>
+    ${product?.Colors ? `
+    <div class="my-1 flex">
+      <p class="text-sm font-medium text-gray-700 mr-2">Colors:</p>
+      <div class="flex items-center">
+        ${product.Colors.map(color => `
+          <span class="w-4 h-4 rounded-full border mr-2" style="background-color: ${color};"></span>
+        `).join('')}
+      </div>
+    </div>
+  ` : ""}
+  ${product?.Sizes ? `
+    <div class="my-2 flex justify-start items-center">
+      <p class="text-sm font-medium text-gray-700 mr-2">Sizes:</p>
+      <div class="flex items-center">
+        ${product.Sizes.map(size => `
+          <span class="px-2 text-sm bg-gray-400 rounded-md border border-gray-500 mr-2">${size}</span>
+        `).join('')}
+      </div>
+    </div>
+  ` : ""}
+  </div>
+  <button class="mt-4 w-full bg-blue-500 text-white py-2 shadow hover:bg-blue-600 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition ease-in-out duration-300 absolute bottom-0">
+    Add to Cart
+  </button>
+</div>
+
     `;
   });
 }
